90055210419dee9d6c4362ef7c04aa7ce2b44192
lucashthiele <lucasthiele0@outlook.com>
2025-06-11 14:11:38 -0300

    healthz endpoint

---
+++ Controllers/HealthzController.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+  [Route("/api/healthz")]
+  [ApiController]
+  public class HealthzController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult HealthzHandler()
+    {
+      return Ok("everything working");
+    }
+  }
+}
